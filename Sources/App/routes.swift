94af33f42055e61b5f692214f77e2c8bbb88cd1f
andrekilik <a.casarini@gmail.com>
2018-05-16 15:49:20 -0300

    Moved routes to Controllers

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,74 +7,10 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
-    
-    // Insere um novo acronimo na tabela
-    router.post("api", "acronyms") { req -> Future<Acronym> in
-        return try req.content.decode(Acronym.self)
-            .flatMap(to: Acronym.self) { acronym in
-                // 3
-                return acronym.save(on: req)
-        }
-    }
 
-    // executa um select *
-    router.get("api", "acronyms") { req -> Future<[Acronym]> in
-        return Acronym.query(on: req).all()
-    }
-    
-    // executa um select acronym where
-    router.get("api", "acronyms", Acronym.parameter) { req -> Future<Acronym> in
-        return try req.parameters.next(Acronym.self)
-    }
-    
-    // executa Update
-    router.put("api", "acronyms", Acronym.parameter) { req -> Future<Acronym> in
-        return try flatMap(to: Acronym.self,
-                           req.parameters.next(Acronym.self),
-                           req.content.decode(Acronym.self)) {
-                            acronym, updatedAcronym in
-                            acronym.short = updatedAcronym.short
-                            acronym.long = updatedAcronym.long
-                            return acronym.save(on: req)
-        }
-    }
-    
-    // executa um delete
-    router.delete("api", "acronyms", Acronym.parameter) { req -> Future<HTTPStatus> in
-        return try req.parameters.next(Acronym.self).delete(on: req).transform(to: HTTPStatus.noContent)
-    }
-    
-    // performs search
-    router.get("api", "acronyms", "search") { req -> Future<[Acronym]> in
-        guard let searchTerm = req.query[String.self, at: "term"] else {
-            throw Abort(.badRequest)
-        }
-        // Busca um parametro individual
-        // return try Acronym.query(on: req)
-            //.filter(\.short == searchTerm)
-        // busca multiplos parametros
-        return try Acronym.query(on: req).group(.or) { or in
-            try or.filter(\.short == searchTerm)
-            try or.filter(\.long == searchTerm)
-            }.all()
-    }
-    
-    // returns the first
-    router.get("api", "acronyms", "first") { req -> Future<Acronym> in
-        return Acronym.query(on: req).first()
-            .map(to: Acronym.self) { acronym in
-                guard let acronym = acronym else {
-                    throw Abort(.notFound)
-                }
-         return acronym
-        }
-    }
-    // sort the results
-    router.get("api", "acronyms", "sorted") { req -> Future<[Acronym]> in
-        return try Acronym.query(on: req)
-                    .sort(\.short, .ascending)
-                    .all()
-    }
+    // Controller configuration
+    let acronymsController = AcronymsController()
+    try router.register(collection: acronymsController)
     
     // Example of configuring a controller
 //    let todoController = TodoController()
